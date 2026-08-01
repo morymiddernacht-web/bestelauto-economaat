00efa33e05726e9bd9447ac9348f10b2abefe408
morymiddernacht-web <morymiddernacht@gmail.com>
2026-08-01 22:05:40 +0200

    Add files via upload

---
+++ config 2.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = "https://twypyfihcymqwunlaxad.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_3PF0WSTPoMpUbXPAS2-1EQ_iCKEGFJp";
+
+// Wachtwoord voor de Beheer-pagina (verander dit gerust)
+const ADMIN_PASSWORD = "verander-mij";
